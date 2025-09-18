2b4e54ed600e4eab5ce7930ff4ebccc71158c45d
Luctiiw <nattangg27@gmail.com>
2025-09-18 13:03:11 -0400

    Corrigindo o caminho do cache para deploy na Heroku

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,12 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Sentry\Laravel\Integration;
 
-return Application::configure(basePath: dirname(__DIR__))
+return tap(Application::configure(basePath: dirname(__DIR__)), function (Application $app) {
+    if (env('APP_ENV') === 'production') {
+        $app->useStoragePath('/tmp/storage');
+        $app->useTemporaryPath('/tmp/cache');
+    }
+})
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
